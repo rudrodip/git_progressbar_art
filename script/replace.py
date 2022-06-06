3cbb60e260a42f6d1ca7b143a4aa21bf18e5e793
Rudrodip <official.rudrodipsarker@gmail.com>
2022-06-06 17:55:44 +0600

    updated

--- script/replace.py
+++ script/replace.py
@@ -5,7 +5,9 @@ def replace_at_index(text, coord):
     pat = re.compile(fr'translate\({y}, 0\).+?rect.+?y=.+?{x}.+?data-level=\"(\d+)', re.S)
     new = pat.search(text)
     index = new.span(1)[0]
-    text = text[:index] + str(z) + text[index + 1:]
+    text = list(text)
+    text[index] = str(z)
+    text = ''.join(text)
     return text
 
 def replace_html(html, coords):
