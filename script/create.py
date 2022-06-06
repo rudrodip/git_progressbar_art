ac5dcafd37715a34ee4137ee6e4b52c48d8f301e
Rudrodip <official.rudrodipsarker@gmail.com>
2022-06-06 19:18:35 +0600

    added error handling

--- script/create.py
+++ script/create.py
@@ -2,6 +2,9 @@ from script import get_html, algorithm, replace
     
 def main(link, text):
     content = get_html.page_content(link)
-    coords = algorithm.coordinates(text)
-    replaced_content = replace.replace_html(content, coords)
+    try:
+        coords = algorithm.coordinates(text)
+        replaced_content = replace.replace_html(content, coords)
+    except:
+        return content
     return replaced_content
