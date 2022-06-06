99a274cf59c631d1d3c5ebdb54e1b1e5c4658d37
Rudrodip <official.rudrodipsarker@gmail.com>
2022-06-06 18:42:12 +0600

    test

--- script/create.py
+++ script/create.py
@@ -3,5 +3,5 @@ from script import get_html, algorithm, replace
 def main(link, text):
     content = get_html.page_content(link)
     coords = algorithm.coordinates(text)
-    # replaced_content = replace.replace_html(content, coords)
-    return content
+    replaced_content = replace.replace_html(content, coords)
+    return replaced_content
